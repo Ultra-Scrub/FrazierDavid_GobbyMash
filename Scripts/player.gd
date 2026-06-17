da7b99c8baf6ed6ff74ea5ba81b252436e4f41cb
Ultra-Scrub <Scottybright1997@gmail.com>
2026-06-16 17:56:03 -0700

    Initial commit

---
+++ Scripts/player.gd
@@ -0,0 +1,71 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+#get the gravity from the project settings to be synced with RigidBody nodes
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var sensivity = 0.003
+@onready var camera = $Firstperson
+@onready var animationplayer = $AnimationPlayer
+@onready var cooldown = $AttackCoooldown
+
+
+func _ready():
+	$Firstperson.current = true
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _switch_veiw():
+	if Input.is_action_just_pressed("switch"):
+		if camera == $Firstperson:
+			camera = $Head
+			$Head/Thirdperson.current = true
+		else:
+			camera = $Firstperson
+			$Firstperson.current = true
+
+func attack():
+	if Input.is_action_just_pressed("attack") and cooldown == false:
+		animationplayer.play("SwordSwing")
+		cooldown = true
+		cooldown.start()
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * sensivity)
+		camera.rotate_x(-event.relative.y * sensivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(70))
+
+func _process(_delta):
+	attack()
+	_switch_veiw()
+	if Input.is_action_just_pressed("escape"):
+		get_tree().quit()
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+
+func _on_attack_coooldown_timeout() -> void:
+	pass # Replace with function body.
